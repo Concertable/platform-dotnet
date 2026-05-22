f812c2ee1c98a1b15a950ad2d14f78da0f9e01d4
Tommy Seery <Tommy.Seery@infonetica.net>
2026-05-22 21:17:33 +0100

    Move IReadDbContext + ReadDbContext into Concertable.B2B.DataAccess
    IReadDbContext lived in Concertable.DataAccess.Application (a shared lib), which gave
    Customer, Payment, and Search services transitive access to all B2B data entities.
    ReadDbContext is B2B-only and belongs in the B2B service boundary.
    
    New project Concertable.B2B.DataAccess owns IReadDbContext, ReadDbContext, and
    AddReadDbContext; DataAccess.Infrastructure retains AddDataAccessSpecifications for
    the internal UpcomingSpecification/DateRangeSpecification registrations.

--- src/Concertable.DataAccess/Concertable.DataAccess.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/Concertable.DataAccess/Concertable.DataAccess.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,21 +1,13 @@
 using Concertable.DataAccess.Infrastructure.Specifications;
 using Concertable.DataAccess.Specifications;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Concertable.DataAccess.Infrastructure.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddReadDbContext(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddDataAccessSpecifications(this IServiceCollection services)
     {
-        services.AddDbContext<ReadDbContext>(opt =>
-            opt.UseSqlServer(
-                configuration.GetConnectionString("B2BDb"),
-                sqlOpt => sqlOpt.UseNetTopologySuite()));
-        services.AddScoped<IReadDbContext, ReadDbContext>();
-
         services.AddScoped(typeof(IUpcomingSpecification<>), typeof(UpcomingSpecification<>));
         services.AddScoped(typeof(IDateRangeSpecification<>), typeof(DateRangeSpecification<>));
         return services;
