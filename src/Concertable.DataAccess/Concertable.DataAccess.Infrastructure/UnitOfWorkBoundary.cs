685ed9e03f89dd31fd777f6e52dd77261baf23eb
Tommy Seery <Tommy.Seery@infonetica.net>
2026-08-27 10:28:09 +0100

    Use UoW boundaries for Concert settlement

---
+++ src/Concertable.DataAccess/Concertable.DataAccess.Infrastructure/UnitOfWorkBoundary.cs
@@ -0,0 +1,30 @@
+using Concertable.DataAccess.Application;
+using Microsoft.EntityFrameworkCore;
+
+namespace Concertable.DataAccess.Infrastructure;
+
+public class UnitOfWorkBoundary<TContext>(IDbContextFactory<TContext> dbContextFactory)
+    : IUnitOfWorkBoundary<TContext>
+    where TContext : DbContextBase
+{
+    public Task ExecuteAsync(
+        Func<TContext, Task> operation,
+        CancellationToken cancellationToken = default) =>
+        ExecuteAsync(async context =>
+        {
+            await operation(context);
+            return true;
+        }, cancellationToken);
+
+    public async Task<TResult> ExecuteAsync<TResult>(
+        Func<TContext, Task<TResult>> operation,
+        CancellationToken cancellationToken = default)
+    {
+        await using var context = await dbContextFactory.CreateDbContextAsync(cancellationToken);
+        var unitOfWork = new UnitOfWork<TContext>(context);
+
+        return await unitOfWork.ExecuteAsync(
+            () => operation(context),
+            cancellationToken);
+    }
+}
